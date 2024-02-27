45c06a46bfa1bb6cd808765f37fba887609214f4
David Starr <david@elegantcode.com>
2024-02-27 14:08:45 -0800

    Any vs. Count for lists

---
+++ src/LanguagePatternsTest/Perfomance/AnyVsCount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguagePatternsTest.Perfomance;
+public class AnyVsCount
+{
+    private List<string> people;
+
+    public AnyVsCount()
+    {
+        people = new List<string>()
+        {
+            "David",
+            "John",
+            "Peter",
+            "Paul",
+            "Mary",
+            "Jane",
+            "Lucy",
+            "Mark",
+            "Luke",
+            "Matthew",
+        };
+    }
+
+    [Fact]
+    public void Any()
+    {
+        var result = people.Any(p => p == "John");
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void Count()
+    {
+        var result = people.Count(p => p == "John") > 0;
+        Assert.True(result);
+    }
+}
