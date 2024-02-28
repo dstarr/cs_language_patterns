9b8770bf15de22b1d15af5c8522115f83f480e22
David Starr <dastarr@microsoft.com>
2024-02-28 07:36:43 -0800

    spread operator tests

---
+++ src/LanguagePatternsTest/Sugar/SpreadOperator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguagePatternsTest.Sugar;
+
+public class SpreadOperator
+{
+    readonly int[] _first = { 1 ,2, 3 };
+    readonly int[] _second = { 4, 5, 6 };
+    readonly int[] _third = { 7, 8, 9 };
+    
+    [Fact]
+    public void JoinListsWithConcat()
+    {
+        int[] singleArray = _first.Concat(_second).Concat(_third).ToArray();
+        Assert.Equal(9, singleArray.Length);
+    }
+
+    //[Fact]
+    public void JoinListsWithSpreadOperator()
+    {
+        //int[] singleArray = [ .._first, .._second, .._third ];
+    }
+}
