7683dadbb6b130b9dee820eda4d8a1701d9697bf
David Starr <david@elegantcode.com>
2024-02-29 09:32:49 -0800

    AnyVsContains

--- src/LanguagePatternsTest/Performance/AnyVsContains.cs
+++ src/LanguagePatternsTest/Performance/AnyVsContains.cs
@@ -5,6 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace LanguagePatternsTest.Performance;
-internal class AnyVsContains
+
+public class AnyVsContains
 {
+    private readonly int[] _numbers;
+
+    public AnyVsContains()
+    {
+        _numbers = new int[100];
+        for (int i = 1; i <= 100; i++)
+        {
+            _numbers[i-1] = i;
+        }
+    }
+
+    [Fact]
+    public void Any()
+    {
+        bool any5 = _numbers.Any(n => n == 5);
+        Assert.True(any5);
+    }
+
+    [Fact]
+    public void Contains()
+    {
+        bool contains5 = _numbers.Contains(5);
+        Assert.True(contains5);
+    }
 }
