6daf88d575a3dfd398333b2409bb47fb15223c87
David Starr <dastarr@microsoft.com>
2024-03-10 15:03:39 -0700

    added method tests for takewhile and where

---
+++ src/LanguagePatternsTest/Sugar/TakeWhileMethod.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguagePatternsTest.Sugar;
+
+public class TakeWhileMethod
+{
+    private readonly List<string> _list;
+
+    public TakeWhileMethod()
+    {
+        _list = new List<string>
+        {
+            "David",
+            "Dani",
+            "Don",
+            "Alberto",
+            "Benito",
+            "Carlos",
+            "Dragon",
+            "Eva",
+            "Felipe",
+            "Gustavo",
+            "Hugo",
+            "Ivan",
+            "Jorge",
+            "Kevin",
+            "Luis",
+            "Miguel",
+            "Nestor",
+            "Oscar",
+        };
+    }
+
+    [Fact]
+    public void WhereMethodTest()
+    {
+        Assert.Equal(18, _list.Count);
+
+        var result = _list
+            .Where(x => x.StartsWith('D'))
+            .ToList();
+        
+        Assert.Equal(4, result.Count);
+        Assert.Equal("Don", result[2]);
+    }
+
+    [Fact]
+    public void TakeWhileMethodTest()
+    {
+        Assert.Equal(18, _list.Count);
+
+        var result = _list
+            .TakeWhile(x => x.StartsWith('D'))
+            .ToList();
+
+        Assert.Equal(3, result.Count);
+        Assert.Equal("Don", result[2]);
+    }
+
+}
